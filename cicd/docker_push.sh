160c473e2192cb0570c18e59cd5da2b6eae3ae2a
Nick Mitchell <nick@kitset.io>
2019-01-29 17:21:51 +1100

    Changes

--- cicd/docker_push.sh
+++ cicd/docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push superlogical/docker-nodejs-swagger-travisci
+docker push superlogical/docker-nodejs-swagger-travisci:$TRAVIS_BUILD_NUMBER
